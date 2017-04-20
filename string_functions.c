ce58bbd2fdd2022cc4a9a4e9ace20a63fd06bc92
kedpak <kedpak@yahoo.com>
2017-04-20 05:26:09 +0000

    fixed betty issues

--- string_functions.c
+++ string_functions.c
@@ -1,16 +1,27 @@
 #include "holberton.h"
 
+/**
+ * _strlen - find length of string
+ * @s: input string
+ * Return: length integer
+ */
 int _strlen(const char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
-        return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
+/**
+ * _strcpy - copy strings
+ * @dest: destnation output
+ * @src: src input
+ * Return: destination of copied string
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
@@ -22,34 +33,37 @@ char *_strcpy(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
-
 	return (dest);
 
 }
 
+/**
+ * _strcat - concatanate two strings
+ * @dest: destination output
+ * @src: src input
+ * Return: concatonated strings
+ */
 char *_strcat(char *dest, char *src)
 {
-        int i, j;
+	int i, j;
 
-        i = 0;
-        j = 0;
-
-        /* gets the length of src string*/
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-        dest[i] = '/';
-        i++;
-        while (src[j] != '\0')
-        {
-
-                dest[i] = src[j];
-                j++;
-                i++;
-        }
-        dest[i] = '\0';
-        return (dest);
+	i = 0;
+	j = 0;
+/* gets the length of src string*/
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	dest[i] = '/';
+	i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 /**
